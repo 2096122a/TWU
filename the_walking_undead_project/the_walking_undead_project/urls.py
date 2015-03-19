0030fc91deb86f7a31bd0110b2b960b98ae3ccfd
2074214y <theorouskata@gmail.com>
2015-03-19 18:19:23 +0000

    small urls change

--- the_walking_undead_project/the_walking_undead_project/urls.py
+++ the_walking_undead_project/the_walking_undead_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls import patterns, url
-from rango import views
+from twu import views
 from django.conf import settings
 from django.conf.urls.static import static
 from registration.backends.simple.views import RegistrationView
