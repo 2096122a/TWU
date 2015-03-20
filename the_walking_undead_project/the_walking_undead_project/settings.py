1d5711cab19e7605105f78826695962a3e0f1bba
unknown <2074214y@BO715-3-05.ad.dcs.gla.ac.uk>
2015-03-20 17:14:45 +0000

    fixed the registration

--- the_walking_undead_project/the_walking_undead_project/settings.py
+++ the_walking_undead_project/the_walking_undead_project/settings.py
@@ -100,8 +100,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PASSWORD_HASHERS = (
-        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-'django.contrib.auth.hashers.BCryptPasswordHasher',
 'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 )
