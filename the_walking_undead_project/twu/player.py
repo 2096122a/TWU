f6f49b5e0b529e78bbd0726aaacf0fa7d7894e5c
Ivaylo Valkov <2064491V@student.gla.ac.uk>
2015-03-25 15:49:52 +0000

    Commented player and tile classes

--- the_walking_undead_project/twu/player.py
+++ the_walking_undead_project/twu/player.py
@@ -1,9 +1,16 @@
+# Holds all of the information for the character of each player, i.e:
+# weapons available, health, weapon used. Also handles actions like decrementing
+# health and switching active weapon used.
 class Player:
 
+    # A collection of all ranged weapons potentially available to the player
     weapons_ranged = { 'pistol': 3, 'shotgun': 2 }
 
+    # A collection of all melee weapons potentially available to the player
     weapons_melee = { "bat": 5, "chainsaw": 4 }
 
+    # Instantiates a new player with 30 health, an old revolver with 6 bullets
+    # and with a score of 0
     def __init__(self):
         self.health = 30
         self.bullets = 6
@@ -11,34 +18,32 @@ class Player:
         self.ranged_power = 3
         self.melee_name = "bat"
         self.melee_power = 5
-        self.ranged_used = True
+        self.ranged_used = True #True=ranged weapon is used, False=melee used
         self.score = 0
 
-
+    # If ranged weapon supplied is in the list of supported ones,
+    # makes the player use that weapon
     def set_ranged(self, weapon_name):
         if weapon_name in weapons_ranged:
             self.ranged_name = weapon_name
             self.ranged_power = weapons_ranged[weapon_name]
 
+    # If melee weapon supplied is in the list of supported ones,
+    # makes the player use that weapon
     def set_melee(self, weapon_name):
         if weapon_name in weapons_melee:
             self.melee_name = weapon_name
             self.melee_power = weapons_melee[weapon_name]
 
+    # Switches whether the player uses their melee or ranged weapon
     def switch_active(self):
         self.ranged_used = not self.ranged_used
 
+    # Decrements player's health by the given amount
+    # If damage is more than the players remaining health
+    # a PlayerIsDead exception is raised
     def lose_health(self, damage):
         if self.health > damage:
             self.health -= damage
         else:
             raise PlayerIsDead("Game Over")
-
-##i *think* we need am edit to the zombie attack along the lines of
-##"if after zombie attack, player health <1, throw exception" and
-##the catch exception causes the pop up?
-##might need ajax for that. chapter 19 in tango book
-
-##    def check_death(self):
-##        if self.health < 1:
-##            raise ValueException("Player has died!")
--- the_walking_undead_project/twu/tile.py
+++ the_walking_undead_project/twu/tile.py
@@ -1,7 +1,10 @@
 import random
 
+# Keeps a record of all of the information associated with each tile of the map
+# i.e: connections available, whether tile is uncovered and number of zombies on that tile.
 class Tile:
 
+    # Instantiates a new non-uncovered tile with no zombies on it
     def __init__(self): # 0 == False == no connection
         self.up = False # 1 == True == connection exists
         self.down = False
@@ -10,23 +13,29 @@ class Tile:
         self.uncovered = False # False = tile has not been visited yet
         self.zombies = 0
 
+    # Custom casts each tile to sting, used to represent map in HTML
+    # Each tile is given a number between 0 and 15
     def __str__(self):
         if self.uncovered:
             return str(8*int(self.up)+4*int(self.down)+2*int(self.left)+int(self.right))
         else:
-            return "16"
+            return "16" # 'Escape code' for a non-uncovered tile
 
     def uncover(self):
         self.uncovered = True
 
+    # Generates a new tile based on directions provided
+    # Directions are an ordered list [UP,DOWN,LEFT,RIGHT]
+    # If a direction is not specified (i.e. None) the connection is
+    # randomly generated
     def generate(self,directions):
         for i in range(4):
             if directions[i] == None:
-                directions[i] = (random.randint(1,3)<3)
+                directions[i] = (random.randint(1,3)<3) #66.(6)% chance to have a connection
         self.up = directions[0]
         self.down = directions[1]
         self.left = directions[2]
         self.right = directions[3]
+        # Adds a random number of zombies on the newly generated tile
         self.zombies = (random.randint(1,2))
-        self.uncover()
-
+        self.uncover() # The tile is now uncovered
