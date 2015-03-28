18a3964a82ef81cf9e5604e7cc45a6658a239ed1
Ivaylo Valkov <2064491V@student.gla.ac.uk>
2015-03-28 19:54:13 +0000

    Fixed game to not crash on death

--- the_walking_undead_project/static/ajax/twu-ajax.js
+++ the_walking_undead_project/static/ajax/twu-ajax.js
@@ -43,24 +43,14 @@ $(document).ready( function() {
 		}
         });
     });
-
-    $("#newdice").click( function(event) {
-		$.get('/twu/game/player_attack/', {}, function(data){
-			$("#dice").html(data);
-			$("#text_box").html(data);
-
-        });
-
-    });
 	
-	$("#dice").click( function(event) {
+    $("#dice").click( function(event) {
 		var number = 1 + Math.floor(Math.random() * 6);
         $.get('/twu/game/roll_dice/', { roll : number}, function(data){
             $("#dice").html(data);
-			});
-		$.get('/twu/game/player_attack/', {damage : number}, function(data2){
-			$("#text_box").html(data2);
-
+	});
+	$.get('/twu/game/player_attack/', {damage : number}, function(data2){
+		$("#text_box").html(data2);
         });
 
     });
@@ -76,33 +66,49 @@ $(document).ready( function() {
     });
 
 
+	$("#newmap").click( function(event) {
+		$.get('/twu/game/map_refresh/', {}, function(data){
+		location.href = ('/twu/game/')	 
+        	});
+	});
+
     $(document).keydown(function(e){ 
 	if (e.keyCode == 87) 
 		$.get('/twu/game/move/', {direction: "up"}, function(data){ 
-		$("#game_screen").html(data); 
-	}); 
+			if (data == "1") {
+				location.href = ('/twu/game/gameover/')
+			}
+			else {
+				$("#game_screen").html(data);
+			}
+		}); 
 	if (e.keyCode == 65) 
 		$.get('/twu/game/move/', {direction: "left"}, function(data){ 
-		$("#game_screen").html(data); 
-	}); 
+			if (data == "1") {
+				location.href = ('/twu/game/gameover/')
+			}
+			else {
+				$("#game_screen").html(data);
+			}
+		});
 	if (e.keyCode == 68) 
-		$.get('/twu/game/move/', {direction: "right"}, function(data){ 
-		$("#game_screen").html(data); 
-	}); 
+		$.get('/twu/game/move/', {direction: "right"}, function(data){
+			if (data == "1") {
+				location.href = ('/twu/game/gameover/')
+			}
+			else {
+				$("#game_screen").html(data);
+			}
+		});
 	if (e.keyCode == 83) 
 		$.get('/twu/game/move/', {direction: "down"}, function(data){ 
-		$("#game_screen").html(data);
-	}); 
-    });
-	
-
-
-
-	 $("#newmap").click( function(event) {
-		$.get('/twu/game/map_refresh/', {}, function(data){
-		location.href = ('/twu/game/')	 
-        });
-
+			if (data == "1") {
+				location.href = ('/twu/game/gameover/')
+			}
+			else {
+				$("#game_screen").html(data);
+			}
+		});
     });
 
 });
